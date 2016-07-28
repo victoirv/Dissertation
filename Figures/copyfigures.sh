4a38f0d2b96248e5ff502fd9e2b943c54525b65c
victoirv <victoirv@gmail.com>
2016-07-28 13:03:49 -0400

    Presentation figures

---
+++ Figures/copyfigures.sh
@@ -0,0 +1,5 @@
+for i in *.eps; do find ~/Work/Density/ -iname $i -exec cp {} . \; ; done
+cd StormAvs
+for i in *.eps; do find ~/Work/Density/ -iname $i -exec cp {} . \; ; done
+cd ../CH5
+for i in *.eps; do find ~/Work/Density/ -iname $i -exec cp {} . \; ; done
